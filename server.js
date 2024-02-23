2431c9a5c276999576722ae3f97b84b4ebb4d64f
GAFerreira13 <gon.skater@hotmail.com>
2024-02-23 04:28:00 +0000

    asdasd

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const stripe = require('stripe')(API_KEY);
+const stripe = require('stripe')('API_KEY');
 const cors = require('cors');
 
 // Middleware to enable CORS ...
