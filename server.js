a6e0c381f3a0dbfed91efe270fae97d146c88fce
GAFerreira13 <gon.skater@hotmail.com>
2024-02-28 04:25:07 +0000

    updates

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const YOUR_DOMAIN = 'https://fluidinova.webflow.io';
 
 // Endpoint to create checkout session
 app.post('/create-checkout-session', async (req, res) => {
-    const { name, email, taxID } = req.body;
+    const { customer, shippingAddress, cartItems } = req.body;
 
     /*if (!amount || !currency || !productName || !productImage || !successUrl || !cancelUrl) {
         return res.status(400).json({ error: 'Missing required parameters' });
@@ -23,36 +23,34 @@ app.post('/create-checkout-session', async (req, res) => {
     try {//so depois das verificacoes todas...
         const session = await stripe.checkout.sessions.create({
             //payment_method_types: ['card'],
-            line_items: [
-                {
-                  // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-                  price: 'price_1Ok68tI6TgsJpVaUh50ZW7jc',//EXEMPLOOOOOOOOOO
-                  quantity: 1,
-                },
-                {
-                    // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-                    price: 'price_1Ok62kI6TgsJpVaU1Ray51Pa',//EXEMPLOOOOOOOOOO
-                    quantity: 1,
-                },
-                {
-                    // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-                    price: 'price_1Ok62RI6TgsJpVaUpjjQ5EuJ',//EXEMPLOOOOOOOOOO
-                    quantity: 1,
-                  },
-              ],
+            customer_email: customer.email,
+            submit_type: 'auto',
+            billing_address_collection: 'auto',
+            
+            line_items: cartItems.map(item => ({
+                price: item.price,
+                quantity: item.quantity,
+                tax_rates: item.tax_rates
+            })),
               mode: 'payment',
               success_url: 'https://fluidinova.webflow.io/success',
               cancel_url: 'https://fluidinova.webflow.io/cancel',
+              allow_promotion_codes: true,
+              automatic_tax: {
+                enabled: false
+            },
+            billing_address_collection: 'required',
+            customer_creation: 'always',
+            tax_id_collection: {
+                enabled: true
+            }
+              
             });
 
-        //res.status(200).json({ sessionId: session.id });
-        //res.redirect(303, session.url);
+        
         res.json({ url: session.url })
         
-        // Access the URL from the session's payment_intent object
-       // const url = session.payment_intent.charges.data[0].receipt_url;
-
-       // res.json({ url });
+        
 
     } catch (error) {
         console.error('Error creating checkout session:', error);
