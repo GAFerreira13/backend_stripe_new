ee98abae387dc07ba3b6816f65d7a8d86c8cf038
GAFerreira13 <gon.skater@hotmail.com>
2024-03-01 02:26:15 +0000

    replace fetch with axios

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 const stripe = require('stripe')(process.env.API_KEY);
+const axios = require('axios').default;
 const cors = require('cors');
-const fetch = require('node-fetch');
 
 app.use(cors());
 app.use(express.json());
@@ -13,25 +13,23 @@ app.post('/validate-eori', async (req, res) => {
     const { eori } = req.body;
 
     try {
-        const response = await fetch('https://api.service.hmrc.gov.uk/customs/eori/lookup/check-multiple-eori', {
-            method: 'POST',
+        const response = await axios.post('https://api.service.hmrc.gov.uk/customs/eori/lookup/check-multiple-eori', {
+            eoris: [eori]
+        }, {
             headers: {
                 'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ eoris: [eori] })
+            }
         });
 
-        if (!response.ok) {
+        if (!response.data || !response.data.status) {
             throw new Error('Failed to validate EORI');
         }
 
-        const data = await response.json();
-
-        if (data.status === 200) {
+        if (response.data.status === 200) {
             res.status(200).json({ message: "EORI - Success!" });
-        } else if (data.status === 400) {
+        } else if (response.data.status === 400) {
             res.status(400).json({ message: "EORI - Invalid number, cannot purchase as business" });
-        } else if (data.status === 600) {
+        } else if (response.data.status === 600) {
             res.status(500).json({ message: "EORI - Server error. Please contact admin" });
         }
     } catch (error) {
@@ -44,19 +42,13 @@ app.get('/validate-vat/:vat_number', async (req, res) => {
     const { vat_number } = req.params;
 
     try {
-        const response = await fetch(`https://api.vatcheckapi.com/v2/check?vat_number=${vat_number}`, {
-            method: 'GET',
+        const response = await axios.get(`https://api.vatcheckapi.com/v2/check?vat_number=${vat_number}`, {
             headers: {
                 'X-Api-Key': 'vat_live_YhSULynHRB5Nae6pJdDqk0IaEr3jUngdReMQxWnQ'
             }
         });
-
-        if (!response.ok) {
-            throw new Error('Failed to validate VAT number');
-        }
-
-        const data = await response.json();
-        res.json(data);
+        res.json(response.data);
+        
     } catch (error) {
         console.error('Error validating VAT number:', error);
         res.status(500).json({ error: 'An error occurred while validating VAT number' });
@@ -109,5 +101,5 @@ app.post('/create-checkout-session', async (req, res) => {
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log('Server is running on port ${PORT}');
+    console.log(`Server is running on port ${PORT}`);
 });
