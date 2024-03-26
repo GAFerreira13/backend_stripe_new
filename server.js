525c0a9de7287fc326695a9b8ea8e50eecd66d74
GAFerreira13 <gon.skater@hotmail.com>
2024-03-26 15:13:47 +0000

    fundo branco

--- server.js
+++ server.js
@@ -373,7 +373,7 @@ const generateOrderSummaryHTML = (cartItems, subtotal) => {
                     max-width: 600px;
                     margin: 0 auto;
                     padding: 20px;
-                    background-color: #f0f0f0;
+                    background-color: #ffffff;
                     border-radius: 5px;
                     box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
                 }
